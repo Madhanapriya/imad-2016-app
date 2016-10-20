7791a211baec09908bc8c3ecadbf2e7a9c961c03
Madhanapriya <madhanapriya1986@gmail.com>
2016-10-20 13:00:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/article one',function(req,res){
+app.get('/article-one',function(req,res){
   res.send('Article one request will be served here');  
     
 });
-app.get('/article two',function(req,res){
+app.get('/article-two',function(req,res){
   res.send('Article two request will be served here');  
     
 });
-app.get('/article three',function(req,res){
+app.get('/article-three',function(req,res){
   res.send('Article three request will be served here');  
     
 });
